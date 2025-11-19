523b23ad39caef686ab7b7ac3adc7b5c506f417f
Wai Yan Phyo <68497838+dev-wyp@users.noreply.github.com>
2025-11-19 15:36:00 +0630

    Update vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,10 +5,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use(express.static('public'));
-
 app.use('/api/auth', require('./routes/auth').default)
-app.use('/api/listings', require('./routes/listings').default)
+app.use('/api/git ', require('./routes/listings').default)
 app.use('/api/categories', require('./routes/categories').default)
 app.use('/api/users', require('./routes/users').default)
 
@@ -20,6 +18,7 @@ console.error(err)
 res.status(err.status || 500).json({ message: err.message || 'Internal' })
 })
 
+app.use(express.static('public'));
 
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log('Server listening on', port))
